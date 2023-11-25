a52987b1a2d078b4619b1b42eab1b4cbb9eff812
Anna <anasoft@yandex.ru>
2023-11-25 12:50:48 +0500

    Task 64

---
+++ Task64/Program.cs
@@ -0,0 +1,10 @@
+﻿void ChainNTo1(int numberN) {
+    if (numberN > 0) {
+        Console.Write($"{numberN} ");
+        numberN--;
+        ChainNTo1(numberN);
+    }
+}
+
+Console.Write("Введите натуральное число N: ");
+ChainNTo1(Convert.ToInt32(Console.ReadLine()));

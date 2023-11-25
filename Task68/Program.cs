1461e1c43cdc8d0ee00b18665549046660680a7c
Anna <anasoft@yandex.ru>
2023-11-25 13:18:48 +0500

    Task 68

---
+++ Task68/Program.cs
@@ -0,0 +1,17 @@
+﻿int AccermansFunction(int numberM, int numberN)
+{
+  if (numberM == 0)
+    return numberN + 1;
+  else
+    if ((numberM != 0) && (numberN == 0))
+      return AccermansFunction(numberM - 1, 1);
+    else
+      return AccermansFunction(numberM - 1, AccermansFunction(numberM, numberN - 1));
+}
+
+Console.Write("Введите неотрицательное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите неотрицательное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"A(m,n) = {AccermansFunction(m, n)}");

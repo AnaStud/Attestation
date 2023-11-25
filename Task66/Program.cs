2e94e02a72d3077c97bc48f17dd93809d7c1f742
Anna <anasoft@yandex.ru>
2023-11-25 13:09:41 +0500

    Task 66

---
+++ Task66/Program.cs
@@ -0,0 +1,17 @@
+﻿int result = 0;
+
+void SumMToN(int numberM, int numberN) {
+    if (numberM < numberN) {
+        numberM++;
+        result += numberM;
+        SumMToN(numberM, numberN);
+    }
+}
+
+Console.Write("Введите натуральное число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+result += M;
+SumMToN(M,N);
+Console.WriteLine(result);
